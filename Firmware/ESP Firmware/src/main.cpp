9c2dd76aec14005cdbe450c6e2ff64c828b3b63f
Mallo321123 <mario@binary-kitchen.de>
2025-05-09 23:52:59 +0200

    esp firmware cleanup

--- Firmware/ESP Firmware/src/main.cpp
+++ Firmware/ESP Firmware/src/main.cpp
@@ -4,130 +4,186 @@
 #include <BLEUtils.h>
 #include <BLE2902.h>
 
-// UUIDs für den BLE-Service und die Charakteristik
-// Diese standardisierten UUIDs funktionieren besser mit verschiedenen Clients
-#define SERVICE_UUID        "6E400001-B5A3-F393-E0A9-E50E24DCCA9E" // UART Service
-#define CHARACTERISTIC_RX   "6E400002-B5A3-F393-E0A9-E50E24DCCA9E" // RX Characteristic (zum Schreiben durch Client)
-#define CHARACTERISTIC_TX   "6E400003-B5A3-F393-E0A9-E50E24DCCA9E" // TX Characteristic (zum Lesen durch Client)
+#define FIRMWARE_VERSION "0.1.0"
+
+#define SERVICE_UUID "6E400001-B5A3-F393-E0A9-E50E24DCCA9E"      // UART Service
+#define CHARACTERISTIC_RX "6E400002-B5A3-F393-E0A9-E50E24DCCA9E" // RX Characteristic
+#define CHARACTERISTIC_TX "6E400003-B5A3-F393-E0A9-E50E24DCCA9E" // TX Characteristic
+
+#define LOOP_SLEEP_TIME 10        // Sleep time in ms
+#define RECONNECT_SLEEP_TIME 1000 // Sleep time in ms
+
+#define SCROLL_UPDATE_INTERVAL 100 // Interval for scroll value update in ms
+
+#define BATTERY_UPDATE_INTERVAL 5000 // Set battery update interval in s
+#define BATTERY_SENSE_PIN 35         // ADC-Pin for Battery Voltage
+#define BATTERY_MAX_VOLTAGE 4.2      // Maximal Battery Voltage
+#define BATTERY_MIN_VOLTAGE 3.3      // Minimal Battery Voltage
+#define BATTERY_VALUE_CORRECTION 1   // Battery value correction
 
 BLEServer *pServer = NULL;
 BLECharacteristic *pTxCharacteristic = NULL;
 bool deviceConnected = false;
 bool oldDeviceConnected = false;
-unsigned long lastSendTime = 0;
-const unsigned long sendInterval = 100; // 2 Sekunden
+unsigned long lastScrollUpdate = 0;
+unsigned long lastBatteryTime = 0;
 
-// Callback für Verbindungsereignisse
-class MyServerCallbacks: public BLEServerCallbacks {
-    void onConnect(BLEServer* pServer) {
-      deviceConnected = true;
-      Serial.println("Client verbunden");
-    };
+// Callback for BLE server connection
+class MyServerCallbacks : public BLEServerCallbacks
+{
+  void onConnect(BLEServer *pServer)
+  {
+    deviceConnected = true;
+    Serial.println("Client connected");
+  };
 
-    void onDisconnect(BLEServer* pServer) {
-      deviceConnected = false;
-      Serial.println("Client getrennt");
-    }
+  void onDisconnect(BLEServer *pServer)
+  {
+    deviceConnected = false;
+    Serial.println("Client disconnected");
+  }
 };
 
-// Callback für empfangene Daten
-class MyCallbacks: public BLECharacteristicCallbacks {
-    void onWrite(BLECharacteristic *pCharacteristic) {
-      std::string rxValue = pCharacteristic->getValue();
+// Callback for received data
+class MyCallbacks : public BLECharacteristicCallbacks
+{
+  void onWrite(BLECharacteristic *pCharacteristic)
+  {
+    std::string rxValue = pCharacteristic->getValue();
 
-      if (rxValue.length() > 0) {
-        Serial.println("Empfangen von Client:");
-        for (int i = 0; i < rxValue.length(); i++) {
-          Serial.print(rxValue[i]);
-        }
-        Serial.println();
-
-        // Echo zurücksenden
-        std::string response = "Empfangen: " + rxValue;
-        pTxCharacteristic->setValue(response);
-        pTxCharacteristic->notify();
+    if (rxValue.length() > 0)
+    {
+      Serial.println("Empfangen von Client:");
+      for (int i = 0; i < rxValue.length(); i++)
+      {
+        Serial.print(rxValue[i]);
       }
+      Serial.println();
     }
+  }
 };
 
-int getScrollValue() {
+int getBatteryLevel()
+{
+  // read raw value from ADC
+  int rawValue = analogRead(BATTERY_SENSE_PIN);
+  float voltage = rawValue * (3.3 / 4095.0) * BATTERY_VALUE_CORRECTION;
+
+  // Pcalculate battery percentage
+  int percentage = map(voltage * 100,
+                       BATTERY_MIN_VOLTAGE * 100,
+                       BATTERY_MAX_VOLTAGE * 100,
+                       0,
+                       100);
+
+  // limit percentage to 0-100
+  percentage = constrain(percentage, 0, 100);
+
+  return percentage;
+}
+
+int getScrollValue()
+{
   return 30;
 }
 
-void setup() {
+void setup()
+{
   Serial.begin(115200);
-  Serial.println("ESP32 BLE UART Sensor");
+  Serial.println("Scroll Wheel version " FIRMWARE_VERSION);
+  Serial.println("Initializing...");
 
-  // BLE initialisieren
-  BLEDevice::init("ESP32_BLEuart");
-  
-  // Server erstellen
+  pinMode(BATTERY_SENSE_PIN, INPUT);
+  analogReadResolution(12);
+
+  // BLE Initialisation
+  BLEDevice::init("Scroll Wheel");
+
+  // Create Server
   pServer = BLEDevice::createServer();
   pServer->setCallbacks(new MyServerCallbacks());
-  
-  // Service erstellen
+
+  // create Service
   BLEService *pService = pServer->createService(SERVICE_UUID);
-  
-  // TX Charakteristik (für Benachrichtigungen an Client)
+
+  // TX Charakteristics
   pTxCharacteristic = pService->createCharacteristic(
-                        CHARACTERISTIC_TX,
-                        BLECharacteristic::PROPERTY_NOTIFY
-                      );
+      CHARACTERISTIC_TX,
+      BLECharacteristic::PROPERTY_NOTIFY);
   pTxCharacteristic->addDescriptor(new BLE2902());
-  
-  // RX Charakteristik (für Befehle vom Client)
+
+  // RX Charakteristcs
   BLECharacteristic *pRxCharacteristic = pService->createCharacteristic(
-                        CHARACTERISTIC_RX,
-                        BLECharacteristic::PROPERTY_WRITE
-                      );
+      CHARACTERISTIC_RX,
+      BLECharacteristic::PROPERTY_WRITE);
   pRxCharacteristic->setCallbacks(new MyCallbacks());
-  
-  // Service starten
+
+  // start service
   pService->start();
-  
-  // Werbung starten
+
+  // Start advertising
   BLEAdvertising *pAdvertising = BLEDevice::getAdvertising();
   pAdvertising->addServiceUUID(SERVICE_UUID);
   pAdvertising->setScanResponse(true);
-  // Diese Einstellungen verbessern die Erkennbarkeit bei iOS und Android
-  pAdvertising->setMinPreferred(0x06);  
-  pAdvertising->setMaxPreferred(0x12);
-  
+
+  // Option for better visibility for android and IOS
+  /*pAdvertising->setMinPreferred(0x06);
+  pAdvertising->setMaxPreferred(0x12);*/
+
   BLEDevice::startAdvertising();
-  Serial.println("BLE UART Gerät bereit, warte auf Verbindung...");
+  Serial.println("Scroll Wheel ready, waiting for client...");
 }
 
-void loop() {
-  // Regelmäßige Sensorwerte senden wenn verbunden
-  if (deviceConnected && (millis() - lastSendTime >= sendInterval)) {
-    lastSendTime = millis();
-    
-    // Sensorwert lesen
+void loop()
+{
+  // Regularly send scroll value
+  if (deviceConnected && (millis() - lastScrollUpdate >= SCROLL_UPDATE_INTERVAL))
+  {
+    lastScrollUpdate = millis();
+
+    // read sensor value
     int value = getScrollValue();
-    Serial.println("Sensorwert: " + String(value));
     char buffer[32];
-    snprintf(buffer, sizeof(buffer), "%d", value);
-    
-    // Wert als Benachrichtigung senden
+    snprintf(buffer, sizeof(buffer), "SCR:%d", value);
+
+    // Send value as notification
     pTxCharacteristic->setValue(buffer);
     pTxCharacteristic->notify();
-    
-    Serial.println("Benachrichtigung gesendet: " + String(buffer));
   }
-  
+
+  // Regularly send battery level
+  if (deviceConnected && (millis() - lastBatteryTime >= BATTERY_UPDATE_INTERVAL))
+  {
+    lastBatteryTime = millis();
+
+    // Read battery level
+    int batteryLevel = getBatteryLevel();
+    char buffer[32];
+    snprintf(buffer, sizeof(buffer), "BAT:%d", batteryLevel);
+
+    // send battery level as notification
+    pTxCharacteristic->setValue(buffer);
+    pTxCharacteristic->notify();
+
+    Serial.printf("Akkuladung: %d%%\n", batteryLevel);
+  }
+
   // Verbindungsstatus verwalten
-  if (!deviceConnected && oldDeviceConnected) {
-    // Wenn Verbindung getrennt wurde
-    delay(500); // Zeit für BLE-Stack zum Bereinigen
-    pServer->startAdvertising(); // Werbung neu starten
-    Serial.println("Werbung neu gestartet");
+  if (!deviceConnected && oldDeviceConnected)
+  {
+    // on reconnect we can start advertising again
+    delay(RECONNECT_SLEEP_TIME);
+
+    pServer->startAdvertising(); // Start advertising
+    Serial.println("BLE advertising started");
     oldDeviceConnected = deviceConnected;
   }
-  
-  // Neue Verbindung erkannt
-  if (deviceConnected && !oldDeviceConnected) {
+
+  // Detect new connections
+  if (deviceConnected && !oldDeviceConnected)
+  {
     oldDeviceConnected = deviceConnected;
   }
-  
-  delay(10); // CPU-Last reduzieren
+
+  delay(LOOP_SLEEP_TIME);
 }
